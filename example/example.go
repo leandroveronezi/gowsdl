97f4692aacac3e05aafb4996113bf5267c51d6d7
leandroveronezi <leandroveronezi@gmail.com>
2020-10-20 09:41:42 -0400

    Alter Module

--- example/example.go
+++ example/example.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/hooklift/gowsdl/example/gen"
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/leandroveronezi/gowsdl/example/gen"
+	"github.com/leandroveronezi/gowsdl/soap"
 )
 
 func ExampleBasicUsage() {
--- example/gen/gen.go
+++ example/gen/gen.go
@@ -4,7 +4,7 @@ import (
 	"encoding/xml"
 	"time"
 
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/leandroveronezi/gowsdl/soap"
 )
 
 // against "unused imports"
